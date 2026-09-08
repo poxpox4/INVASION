a993bd44ed177c69524536d80a67e4f6b1f41875
poxpox4 <nuttapoomkumta2100@gmail.com>
2026-09-08 16:23:08 +0700

    แก้flow การเล่นของปุ่มต่างๆในเกม

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,11 +1,7 @@
-import javax.swing.AbstractAction;
-import javax.swing.JPanel; //พื้นที่สำหรับวาดเกม
-import javax.swing.KeyStroke; //แทนการกดปุ่มคีย์บอร์ด
+import javax.swing.*;//พื้นที่สำหรับวาดเกม
 import java.awt.event.ActionEvent; //รับevent ตอนactionทำงาน
 import java.awt.*; //นำ Class ต่าง ๆ เกี่ยวกับกราฟิกมาใช้
 import java.util.ArrayList;
-import javax.swing.Timer;
-
 public class GamePanel extends JPanel {
     //ตน.
     int playerX = 375;
@@ -19,9 +15,16 @@ public class GamePanel extends JPanel {
     int panelW = 800,panelH = 750;
 
     ArrayList<Bullet> bullets = new ArrayList<>();
+    long lastShotTime = 0; //เก็บเวลาที่เรายิงครั้งล่าสุด
+    int fireDelay = 150; //ต้องรอ 150 มิลลิวินาทีก่อนยิงนัดต่อไป
     // Bullet bullet = new Bullet(); 
     Timer timer;
-    
+    //ตัวแปรเก็บสถานะปุ่ม
+    boolean upPressed = false;
+    boolean downPressed = false;
+    boolean leftPressed = false;
+    boolean rightPressed = false;
+    boolean spacefirepressed = false;
     public GamePanel() {
 
         setPreferredSize(new Dimension(panelW, panelH)); //กำหนดขนาดพื้นที่เกม
@@ -67,69 +70,122 @@ public class GamePanel extends JPanel {
     private void setupKeyBindings() {
         //getInputMap().put()เมื่อกดปุ่ม ให้เรียก Action //.put()เพิ่มข้อมูลเข้าไปข้างใน
         getInputMap(WHEN_IN_FOCUSED_WINDOW)//KeyStroke.getKeyStroke("W") ตรวจปุ่มW
-                .put(KeyStroke.getKeyStroke("W"), "moveUp");
+                .put(KeyStroke.getKeyStroke("pressed W"), "moveUp");
         //getActionMap() ชื่อ Action → ให้ทำอะไร
         //.put("moveUp", ...) กำหนด Action ชื่อ "moveUp" ทำงานตามโค้ดที่อยู่ด้านหลัง ชื่อนี้ต้องตรงกับที่ใส่ใน InputMap
         getActionMap().put("moveUp", new AbstractAction() { //new AbstractAction() สร้าง Action ใหม่ขึ้นมา
             @Override
             public void actionPerformed(ActionEvent e) { //ทำงานเมื่อ Action ถูกเรียก
-                playerY -= playerSpeed;
-                if(playerY<0) playerY=0; //ห้านเกินด้านบน
-                repaint();//วาดใหม่
+                // playerY -= playerSpeed;
+                // if(playerY<0) playerY=0; //ห้านเกินด้านบน
+                // repaint();//วาดใหม่
+                upPressed = true;
+            }
+        });
+        //ตอนปล่อยปุ่ม
+        getInputMap(WHEN_IN_FOCUSED_WINDOW)
+                .put(KeyStroke.getKeyStroke("released W"), "stopUp");
+
+        getActionMap().put("stopUp", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                upPressed = false;
             }
         });
 
-
         getInputMap(WHEN_IN_FOCUSED_WINDOW)
-                .put(KeyStroke.getKeyStroke("S"), "moveDown");
+                .put(KeyStroke.getKeyStroke("pressed S"), "moveDown");
 
         getActionMap().put("moveDown", new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                playerY += playerSpeed;
-                if(playerY+playerHeight>panelH) playerY=panelH-playerHeight; //ห้ามเกินด้านล่าง
-                repaint();
+                // playerY += playerSpeed;
+                // if(playerY+playerHeight>panelH) playerY=panelH-playerHeight; //ห้ามเกินด้านล่าง
+                // repaint();
+                downPressed = true;
+            }
+        });
+        getInputMap(WHEN_IN_FOCUSED_WINDOW)
+                .put(KeyStroke.getKeyStroke("released S"), "stopDown");
+
+        getActionMap().put("stopDown", new AbstractAction() {
+            @Override 
+            public void actionPerformed(ActionEvent e){
+                downPressed = false;
             }
         });
 
-
         getInputMap(WHEN_IN_FOCUSED_WINDOW)
-                .put(KeyStroke.getKeyStroke("A"), "moveLeft");
+                .put(KeyStroke.getKeyStroke("pressed A"), "moveLeft");
 
         getActionMap().put("moveLeft", new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                playerX -= playerSpeed;
-                if(playerX<0)playerX=0; //ห้ามเกินด้านซ้าย
-                repaint();
+                // playerX -= playerSpeed;
+                // if(playerX<0)playerX=0; //ห้ามเกินด้านซ้าย
+                // repaint();
+                leftPressed = true;
+            }
+        });
+        getInputMap(WHEN_IN_FOCUSED_WINDOW)
+                .put(KeyStroke.getKeyStroke("released A"), "stopLeft");
+
+        getActionMap().put("stopLeft", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                leftPressed = false;
             }
         });
 
-
         getInputMap(WHEN_IN_FOCUSED_WINDOW)
-                .put(KeyStroke.getKeyStroke("D"), "moveRight");
+                .put(KeyStroke.getKeyStroke("pressed D"), "moveRight");
 
         getActionMap().put("moveRight", new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                playerX += playerSpeed;
-                if(playerX+playerWidth>panelW)playerX=panelW-playerWidth;
-                repaint();
+                // playerX += playerSpeed;
+                // if(playerX+playerWidth>panelW)playerX=panelW-playerWidth;
+                // repaint();
+                rightPressed = true;
+            }
+        });
+        getInputMap(WHEN_IN_FOCUSED_WINDOW)
+                .put(KeyStroke.getKeyStroke("released D"), "stopRight");
+
+        getActionMap().put("stopRight", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                rightPressed = false;
             }
         });
 
         getInputMap(WHEN_IN_FOCUSED_WINDOW)
-                .put(KeyStroke.getKeyStroke("SPACE"),"shoot");
+                .put(KeyStroke.getKeyStroke("pressed SPACE"),"shoot");
         getActionMap().put("shoot", new AbstractAction() {
             @Override 
             public void actionPerformed(ActionEvent e){
-                shoot();
-                repaint();
+                spacefirepressed = true;
+                // shoot();
+                // repaint();
+            }
+        });
+        getInputMap(WHEN_IN_FOCUSED_WINDOW)
+                .put(KeyStroke.getKeyStroke("released SPACE"), "stopSPACEfire");
+
+        getActionMap().put("stopSPACEfire", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                spacefirepressed = false;
             }
         });
     }
     //method ยิง
     private void shoot() {
+        long currentTime = System.currentTimeMillis();//เวลาปัจจุบันของเครื่อง
+        if (currentTime - lastShotTime < fireDelay) { //ตรวจเวลาระหว่างการยิง
+            return;
+        }
+        lastShotTime = currentTime;
         Bullet bullet = new Bullet();
         int bulletX = playerX + playerWidth / 2 - bullet.width / 2; //กระสุนออกกลางยาน
         int bulletY = playerY;
@@ -137,10 +193,30 @@ public class GamePanel extends JPanel {
     }
     //method update
     private void updateGame() {
-        for (int i = bullets.size()-1;i>=0;i--) {
+        if (upPressed) { //w
+            playerY -= playerSpeed;
+            if(playerY<0) playerY=0;
+        }
+        if (downPressed) { //s
+            playerY += playerSpeed;
+            if(playerY+playerHeight>panelH) playerY=panelH-playerHeight;
+        }
+        if (leftPressed) { //a
+            playerX -= playerSpeed;
+            if(playerX<0)playerX=0;
+        }
+        if (rightPressed) { //d
+            playerX += playerSpeed;
+            if(playerX+playerWidth>panelW)playerX=panelW-playerWidth;
+        }
+        if(spacefirepressed){ //spacefire
+            shoot();
+        }
+        for (int i = bullets.size()-1;i>=0;i--) { //bullets
             Bullet bullet = bullets.get(i);
             bullet.move();
             if(bullet.y+bullet.height<0) bullets.remove(i); //ลบกระสุน
         }
+        
     }
 }
